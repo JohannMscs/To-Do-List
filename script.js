ce77b76360df2fab6d31761b3c85546a4a1eb8c9
Johann Moscoso <110245986+JohannMscs@users.noreply.github.com>
2023-09-18 15:25:58 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 const buttonNewTask = document.querySelector(".new-task button")
+const taskBox = document.querySelector(".task-box")
 buttonNewTask.addEventListener("click", addTask)
+const newTaskInput = document.querySelector(".new-task input")
 
 function addTask() {
+
     event.preventDefault()
-    console.log("add")
-}
+    console.log(newTaskInput.value)
+    const taskItem = document.createElement("div")
+    taskItem.classList.add("task-item")
+
+    const inputItem = document.createElement("input")
+    inputItem.type = "checkbox"
+
+     const taskItemText = document.createElement("p")
+    taskItemText.innerText = newTaskInput.value
+
+    const buttonTrash = document.createElement("button")
+    
+    const trashItem = document.createElement("i")
+    trashItem.classList.add("fa-solid")
+    
+    
+    taskItem.appendChild(inputItem)
+    taskItem.appendChild(taskItemText)
+    taskItem.appendChild(buttonTrash)
+    buttonTrash.appendChild(trashItem)
+    taskBox.appendChild(taskItem)
+
+    newTaskInput.value = ""
+
+} 
